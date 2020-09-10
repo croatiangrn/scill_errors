bb23c58c0ba0e7f6f1aa8ebfe546c0755be8d6e1
Marko Mikulic <mikulic.marko996@gmail.com>
2020-09-10 14:58:26 +0200

    Reorganized errors

--- main.go
+++ main.go
@@ -4,14 +4,26 @@ import "errors"
 
 //goland:noinspection GoUnusedGlobalVariable
 var (
-	GenericErr                = errors.New("generic_err")
-	EmailTaken                = errors.New("email_taken")
+	// Common errors
+	GenericErr = errors.New("generic_err")
+
+	// GDPR Errors
+	DataPrivacyNotApproved   = errors.New("data_privacy_not_approved")
+	PrivacyPolicyNotAccepted = errors.New("privacy_policy_not_accepted")
+
+	// Wargaming platform errors
 	WargamingAccountTaken     = errors.New("wargaming_account_taken")
-	DataPrivacyNotApproved    = errors.New("data_privacy_not_approved")
-	PrivacyPolicyNotAccepted  = errors.New("privacy_policy_not_accepted")
-	UserBlocked               = errors.New("err_user_blocked")
-	UserInactive              = errors.New("err_user_inactive")
-	InvalidEmail              = errors.New("err_invalid_email")
-	RecordNotFound            = errors.New("record_not_found")
+	WargamingAccountIDEmpty   = errors.New("wargaming_account_id_empty")
+	WargamingAccessTokenEmpty = errors.New("wargaming_access_token_empty")
+
+	// User errors
+	UserBlocked  = errors.New("user_blocked")
+	UserInactive = errors.New("user_inactive")
+	InvalidEmail = errors.New("invalid_email")
+	EmailTaken   = errors.New("email_taken")
+
+	// DB Specific errors
+	RecordNotFound = errors.New("record_not_found")
+
 	CouldNotAddGamesToTheList = errors.New("could_not_add_games_to_the_list")
 )
