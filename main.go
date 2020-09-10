1c9f1dd15f86f269ed1f8a82f08b4d7e4faf8686
Marko Mikulic <mikulic.marko996@gmail.com>
2020-09-10 14:51:50 +0200

    Added new errors

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package scill_errors
 
 import "errors"
 
+//goland:noinspection GoUnusedGlobalVariable
 var (
-	ErrRecordNotFound = errors.New("record_not_found")
+	GenericErr                = errors.New("generic_err")
+	EmailTaken                = errors.New("email_taken")
+	WargamingAccountTaken     = errors.New("wargaming_account_taken")
+	DataPrivacyNotApproved    = errors.New("data_privacy_not_approved")
+	PrivacyPolicyNotAccepted  = errors.New("privacy_policy_not_accepted")
+	UserBlocked               = errors.New("err_user_blocked")
+	UserInactive              = errors.New("err_user_inactive")
+	InvalidEmail              = errors.New("err_invalid_email")
+	RecordNotFound            = errors.New("record_not_found")
+	CouldNotAddGamesToTheList = errors.New("could_not_add_games_to_the_list")
 )
