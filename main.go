aa3994110aca2320f42ae1ed600515b4c92e50b9
Marko Mikulic <mikulic.marko996@gmail.com>
2020-09-10 15:38:30 +0200

    Added two new errors

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import "errors"
 //goland:noinspection GoUnusedGlobalVariable
 var (
 	// Common errors
-	GenericErr = errors.New("generic_err")
+	GenericErr   = errors.New("generic_err")
+	InvalidToken = errors.New("invalid_token")
 
 	// GDPR Errors
 	DataPrivacyNotApproved   = errors.New("data_privacy_not_approved")
@@ -17,10 +18,11 @@ var (
 	WargamingAccessTokenEmpty = errors.New("wargaming_access_token_empty")
 
 	// User errors
-	UserBlocked  = errors.New("user_blocked")
-	UserInactive = errors.New("user_inactive")
-	InvalidEmail = errors.New("invalid_email")
-	EmailTaken   = errors.New("email_taken")
+	UserBlocked           = errors.New("user_blocked")
+	UserInactive          = errors.New("user_inactive")
+	InvalidEmail          = errors.New("invalid_email")
+	EmailTaken            = errors.New("email_taken")
+	CouldNotGetUserAvatar = errors.New("could_not_get_user_avatar")
 
 	// DB Specific errors
 	RecordNotFound = errors.New("record_not_found")
